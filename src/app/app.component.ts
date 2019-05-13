56a518eda68a74242fd721a6d7ed744d0a2db5ad
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-05-13 08:28:20 -0300

    chore: add service an add server for jwt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angualar-securing';
+  dangerousCode = 'Template <script>alert("Here")</script>';
 }
